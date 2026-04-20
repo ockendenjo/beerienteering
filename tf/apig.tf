28cf3ca653c2df9697a2155f915c825a2f7bdcce
John Ockenden <29466887+ockendenjo@users.noreply.github.com>
2026-04-20 14:10:32 +0100

    Add lambda and API to put stashes (#4)

--- tf/apig.tf
+++ tf/apig.tf
@@ -1,9 +1,10 @@
 # Define the REST API
 resource "aws_api_gateway_rest_api" "my_api" {
-  name = "hbt-beerienteering"
+  name        = "beerienteering"
+  description = "Beerienteering (${var.env})"
 
   endpoint_configuration {
-    types = ["EDGE"]
+    types = ["REGIONAL"]
   }
 }
 
@@ -14,7 +15,8 @@ resource "aws_api_gateway_deployment" "api" {
     //Note that a deployment can take a few minutes to propagate even after the `terraform apply` has completed
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.root.path_part,
-      aws_api_gateway_method.proxy.id,
+      aws_api_gateway_method.get_stashes.id,
+      aws_api_gateway_method.put_stashes.id,
       module.lambda_get_stashes.invoke_arn,
     ]))
   }
@@ -24,7 +26,8 @@ resource "aws_api_gateway_deployment" "api" {
   }
 
   depends_on = [
-    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.get_stashes,
+    aws_api_gateway_integration.put_stashes,
   ]
 }
 
@@ -42,18 +45,34 @@ resource "aws_api_gateway_resource" "root" {
   path_part   = "stashes"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "get_stashes" {
   rest_api_id   = aws_api_gateway_rest_api.my_api.id
   resource_id   = aws_api_gateway_resource.root.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
+resource "aws_api_gateway_integration" "get_stashes" {
   rest_api_id             = aws_api_gateway_rest_api.my_api.id
   resource_id             = aws_api_gateway_resource.root.id
-  http_method             = aws_api_gateway_method.proxy.http_method
+  http_method             = aws_api_gateway_method.get_stashes.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = module.lambda_get_stashes.invoke_arn
+}
+
+resource "aws_api_gateway_method" "put_stashes" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "put_stashes" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.root.id
+  http_method             = aws_api_gateway_method.put_stashes.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda_put_stashes.invoke_arn
 }
